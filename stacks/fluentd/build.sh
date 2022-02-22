43d3197679a7ea481fb36303df14e34217c184c8
duanhongyi <duanhongyi@doopai.com>
2022-02-22 11:40:41 +0800

    fix(stack): env error

--- stacks/fluentd/build.sh
+++ stacks/fluentd/build.sh
@@ -8,14 +8,15 @@ function build() {
   cat  << EOF > ${PROFILE_DIR}/${STACK_NAME}.sh
 export GEM_HOME=/opt/drycc/fluentd
 export BUNDLE_SILENCE_ROOT_WARNING=1
-export BUNDLE_APP_CONFIG="$GEM_HOME"
-export PATH=$GEM_HOME/bin:$PATH
+export BUNDLE_APP_CONFIG="\$GEM_HOME"
+export PATH=\$GEM_HOME/bin:\$PATH
 EOF
   . ${PROFILE_DIR}/${STACK_NAME}.sh
   ./make.sh
-  echo "export LD_PRELOAD=/usr/lib/libjemalloc.so.2" >> ${PROFILE_DIR}/${STACK_NAME}.sh
+  echo "export LD_PRELOAD=/opt/drycc/fluentd/lib/libjemalloc.so.2" >> ${PROFILE_DIR}/${STACK_NAME}.sh
   cp -rf /opt/drycc/fluentd/* "${DATA_DIR}"
 }
 
 # call build stack
 build-stack "${1}"
+
--- stacks/go/build.sh
+++ stacks/go/build.sh
@@ -8,7 +8,7 @@ function build() {
   generate-stack-path
   curl -L -o /opt/drycc/tmp.tar.gz https://go.dev/dl/go"${STACK_VERSION}".linux-"${OS_ARCH}".tar.gz
   cd /opt/drycc && tar -xvzf tmp.tar.gz && rm -rf tmp.tar.gz && cd -
-  cat  << EOF > "${PROFILE_DIR}"/go.sh
+  cat  << EOF >> "${PROFILE_DIR}"/go.sh
 export GOPATH="/opt/drycc/go"
 EOF
   cp -rf /opt/drycc/go/* ${DATA_DIR}

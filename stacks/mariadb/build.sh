73ce99ed9e7dbeecd8e8dd3e6c50a5e3575bce43
duanhongyi <duanhongyi@doopai.com>
2022-04-29 13:09:54 +0800

    chore(mariadb): update build mariadb

--- stacks/mariadb/build.sh
+++ stacks/mariadb/build.sh
@@ -9,10 +9,8 @@ function build() {
   	cmake \
 
   # Generate binary
-  curl -sSL "https://downloads.mariadb.org/interstitial/mariadb-${STACK_VERSION}/source/mariadb-${STACK_VERSION}.tar.gz" | tar -xz && \
+  curl -sSL "https://archive.mariadb.org/mariadb-${STACK_VERSION}/source/mariadb-${STACK_VERSION}.tar.gz" | tar -xz && \
   cd mariadb-"${STACK_VERSION}" && \
-  pcre_version=$(< cmake/pcre.cmake grep 'ftp.pcre.org' |awk -F 'pcre2-' '{print $2}' | awk -F '.zip' '{print $1}')
-  sed -i -e "s|http://ftp.pcre.org/pub/pcre|https://github.com/PhilipHazel/pcre2/releases/download/pcre2-${pcre_version}|g" cmake/pcre.cmake
   cmake . -DCMAKE_INSTALL_PREFIX=/opt/drycc/mariadb \
   -DMYSQL_DATADIR=/opt/drycc/mariadb/data \
   -DWITH_INNOBASE_STORAGE_ENGINE=1 \
@@ -26,7 +24,7 @@ function build() {
   -DDEFAULT_CHARSET=utf8 \
   -DDEFAULT_COLLATION=utf8_general_ci \
   && \
-  make install
+  make install/strip
 
   mkdir -p "${PROFILE_DIR}"
   cat  << EOF > "${PROFILE_DIR}/${STACK_NAME}.sh"
@@ -38,3 +36,4 @@ EOF
 
 # call build stack
 build-stack "${1}"
+

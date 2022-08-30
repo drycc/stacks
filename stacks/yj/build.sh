b5065aad57f97caf839766cabed1ff0df4afb6ec
duanhongyi <duanhongyi@doopai.com>
2022-08-30 17:28:43 +0800

    fix(stacks): yj not found

--- stacks/yj/build.sh
+++ stacks/yj/build.sh
@@ -8,13 +8,9 @@ function build() {
   generate-stack-path
   BIN_DIR="${DATA_DIR}"/bin
   mkdir -p "${BIN_DIR}"
-  architecture=$(dpkg --print-architecture); \
-  case "$architecture" in \
-    amd64) yj_name=yj-linux ;; \
-    *) yj_name=yj-linux=$architecture ;; \
-  esac ; \
+  architecture=$(dpkg --print-architecture)
   curl -o "${BIN_DIR}"/yj \
-    -L "https://github.com/sclevine/yj/releases/download/v${STACK_VERSION}/$yj_name"; \
+    -L "https://github.com/sclevine/yj/releases/download/v${STACK_VERSION}/yj-linux-$architecture"; \
   chmod +x "${BIN_DIR}"/yj
 }
 

195e64d4029db2763bea213ca743bf8f71429f55
EamonZhang <zhang.eamon@hotmail.com>
2023-11-28 09:09:42 +0800

    fix(cloudbeaver): build error miss jar

--- stacks/cloudbeaver/build.sh
+++ stacks/cloudbeaver/build.sh
@@ -6,23 +6,31 @@
 # Implement build function
 function build() {
   # ref : https://github.com/dbeaver/cloudbeaver/wiki/Build-and-deploy
+  # git tag --sorted=-creatordate --merged remotes/origin/release_23_2_5
   cd /opt/drycc/
   . init-stack
+  JAVA_VERSION=17.0.9
   NODE_VERSION=16.20.1
-  DBEAVER_VERSION=23.2.5
-  install-packages openjdk-17-jdk maven gnupg gnupg2 git
+  DBEAVER_VERSION=23.2.5 #CLOUD_VERSION 23.1.4
+  install-stack java ${JAVA_VERSION}
+  install-packages maven gnupg gnupg2 git
   java --version &&  mvn --version 
   curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" |  tee /etc/apt/sources.list.d/yarn.list
   curl -sL https://deb.nodesource.com/setup_14.x |   bash -
   install-packages yarn
   yarn --version
-  install-stack node $NODE_VERSION
-  export PATH=$PATH:/opt/drycc/node/bin
-  git clone https://github.com/dbeaver/dbeaver.git && cd dbeaver &&  git checkout $DBEAVER_VERSION
+  install-stack node ${NODE_VERSION}
+  export PATH=${PATH}:/opt/drycc/node/bin
+  git clone https://github.com/dbeaver/dbeaver.git 
+  cd dbeaver 
+  git checkout $DBEAVER_VERSION
   cd ..
-  git clone https://github.com/dbeaver/cloudbeaver.git && cd cloudbeaver/ && git checkout $STACK_VERSION
-  cd deploy && cp -r cloudbeaver ${DATA_DIR}
+  git clone https://github.com/dbeaver/cloudbeaver.git 
+  cd cloudbeaver 
+  git checkout ${STACK_VERSION}
+  cd deploy && ./build.sh && cp -r cloudbeaver ${DATA_DIR}
+  
 }
 # call build stack
 build-stack "${1}"

b46de4e183d5770d8ff79dedfebb2011e12b96d8
Eamon <zhang.eamon@hotmail.com>
2024-04-23 16:47:32 +0800

    chore(stacks):add clickhouse
    * feat(stacks): add pgweb
    
    * fix(checher): delete repmgr
    
    * chore(stacks): add cloudbeaver
    
    * chore(stacks): delete pgweb
    
    * chore(stacks): add cloudbeaver
    
    * fix(cloudbeaver): build error miss jar
    
    * fix(cloudbeaver): fix build conflict
    
    * chore(cloudbeaver): delete no used jdk
    
    * chore(stacks): add cloudbeaver
    
    * chore(stacks) add airflow_exporer
    
    * chore(stacks) add mongodb and mongosh
    
    * chore(stacks) delete airflow_exporter
    
    * chore(stacks) delete airflow_exporter checker
    
    * chore(stacks): add mongodb and mongosh
    
    * feat(stacks): add pgweb
    
    * fix(checher): delete repmgr
    
    * chore(stacks): add cloudbeaver
    
    * chore(stacks): delete pgweb
    
    * chore(stacks): add cloudbeaver
    
    * fix(cloudbeaver): build error miss jar
    
    * fix(cloudbeaver): fix build conflict
    
    * chore(cloudbeaver): delete no used jdk
    
    * chore(stacks): add cloudbeaver
    
    * chore(stacks) add airflow_exporer
    
    * chore(stacks) add mongodb and mongosh
    
    * chore(stacks) delete airflow_exporter
    
    * chore(stacks) delete airflow_exporter checker
    
    * chore(mongodb): modify the compilation method in arm environment
    
    * megre(mongodb)
    
    * chore(stacks): add clickhouse

--- scripts/checker.py
+++ scripts/checker.py
@@ -385,6 +385,12 @@ repo_info_table = {
         "owner": "mongodb-js",
         "match": "^v[0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}$",
     },
+    "clickhouse": {
+        "name": "ClickHouse",
+        "type": "github",
+        "owner": "ClickHouse",
+        "match": "^v[0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}.[0-9]{1,}-lts$",
+    },
 }
 
 

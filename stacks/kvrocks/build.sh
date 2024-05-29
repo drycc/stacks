e8162a3aca3b3dcbe63a497fabf742bc5c432d33
duanhongyi <duanhongyi@doopai.com>
2024-05-29 18:49:49 +0800

    fix(kvrocks): add build params

--- stacks/kvrocks/build.sh
+++ stacks/kvrocks/build.sh
@@ -17,7 +17,7 @@ function build() {
   curl -sSL https://github.com/apache/kvrocks/archive/refs/tags/v${STACK_VERSION}.tar.gz | tar xvz
   cd kvrocks-${STACK_VERSION}
   apt install -y git build-essential cmake libtool python3 libssl-dev
-  ./x.py build; cd ..
+  ./x.py build -DENABLE_OPENSSL=ON -DPORTABLE=1 -DCMAKE_BUILD_TYPE=Release; cd ..
   # build kvrocks controller
   install-stack go "${GO_VERSION}" && . init-stack
   KVROCKS_CONTROLLER_GIT_URL=https://github.com/apache/kvrocks-controller

530b360f1e6ca8d03dc4d0d1119b3ff5d6651ff5
duanhongyi <duanhongyi@doopai.com>
2025-04-02 16:15:25 +0800

    chore(registry): change registry to go build

--- stacks/registry/build.sh
+++ stacks/registry/build.sh
@@ -8,10 +8,15 @@ function build() {
   generate-stack-path
   BIN_DIR="${DATA_DIR}"/bin
   mkdir -p "${BIN_DIR}"
-  curl -fsSL -o tmp.tar.gz https://github.com/distribution/distribution/releases/download/v${STACK_VERSION}/registry_${STACK_VERSION}_linux_${OS_ARCH}.tar.gz
-  tar -xzf tmp.tar.gz
-  mv registry "${BIN_DIR}"
-  rm LICENSE README.md tmp.tar.gz
+  install-stack go "${GO_VERSION}"
+  . init-stack
+  curl -fsSL -o tmp.tar.gz https://github.com/distribution/distribution/archive/refs/tags/v${STACK_VERSION}.tar.gz
+  tar -xvzf tmp.tar.gz
+  cd distribution-${STACK_VERSION}
+  go build cmd/registry/main.go
+  mv main "${BIN_DIR}"/registry
+  cd ..
+  rm -rf distribution-${STACK_VERSION} tmp.tar.gz
 }
 
 # call build stack

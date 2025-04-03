36bc51740e4415492d6b8a3703ac0072653e9682
duanhongyi <duanhongyi@doopai.com>
2025-04-03 14:31:04 +0800

    chore(yq): change to golang build

--- stacks/yq/build.sh
+++ stacks/yq/build.sh
@@ -8,10 +8,17 @@ function build() {
   generate-stack-path
   BIN_DIR="${DATA_DIR}"/bin
   mkdir -p "${BIN_DIR}"
-  architecture=$(dpkg --print-architecture)
-  curl -o "${BIN_DIR}"/yq \
-    -L "https://github.com/mikefarah/yq/releases/download/v${STACK_VERSION}/yq_linux_$architecture"; \
-  chmod +x "${BIN_DIR}"/yq
+  install-stack go "${GO_VERSION}"
+  . init-stack
+  curl -fsSL -o tmp.tar.gz https://github.com/mikefarah/yq/archive/refs/tags/v${STACK_VERSION}.tar.gz
+  tar -xvzf tmp.tar.gz
+  cd yq-${STACK_VERSION}
+  # fix CVE-2022-28948
+  go get -u ./...; go mod tidy; go mod vendor
+
+  go build -o "${BIN_DIR}"/yq
+  cd ..
+  rm -rf yq-${STACK_VERSION} tmp.tar.gz
 }
 
 # call build stack
